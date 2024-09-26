e54d6af9ea5411778a23c2ad8c966d5c9a85048d
Денис Червоняк <mestoush@mail.ru>
2024-09-26 08:41:35 +0700

    Вывод страницы лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def start():
-    return "web-server on flask"
+    return """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Червоняк Денис Павлович, лабораторная 1</title>
+</head>
+<body>
+    <header>
+        НГТУ, ФБ, Лабораторная работа 1
+    </header>
+
+    <h1>web-сервер на flask</h1>
+
+    <footer>
+        &copy; Червоняк Денис, ФБИ-24, 3 курс, 2024
+    </footer>
+</body>
+</html>
+"""
