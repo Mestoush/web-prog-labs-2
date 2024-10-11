87ae0a9c0c3ed08d2de70ff22f2a9742bc9fb916
Денис Червоняк <mestoush@mail.ru>
2024-10-11 17:45:15 +0700

    Передача переменных в шаблон

--- app.py
+++ app.py
@@ -209,4 +209,7 @@ def add_flower(name):
 @app.route('/lab2/example')
 def example():
     name = 'Червоняк Денис'
-    return render_template('examples.html', name = name)
+    lab_number = '№2'
+    group = 'ФБИ-24'
+    course = '3'
+    return render_template('examples.html', name = name, lab_number = lab_number, group = group, course = course)
