23d2a1f156649f2ca23c5d756b932ff07068ec22
Денис Червоняк <mestoush@mail.ru>
2024-11-04 22:32:25 +0700

    валидация

--- lab4.py
+++ lab4.py
@@ -103,17 +103,17 @@ def stepen():
 
 tree_count = 0
 
-@lab4.route('/lab4/tree', methods = ['GET', 'POST'])
+@lab4.route('/lab4/tree', methods=['GET', 'POST'])
 def tree():
     global tree_count
-    if request.method == 'GET':
-        return render_template('lab4/tree.html', tree_count = tree_count)
+    if request.method == 'POST':
+        operation = request.form.get('operation')
 
-    operation = request.form.get('operation')
+        if operation == 'cut' and tree_count > 0:
+            tree_count -= 1
+        elif operation == 'plant' and tree_count < 10:
+            tree_count += 1
 
-    if operation == 'cut':
-        tree_count -= 1
-    elif operation == 'plant':
-        tree_count += 1
+    return render_template('lab4/tree.html', tree_count=tree_count)
 
-    return redirect('/lab4/tree')
+
