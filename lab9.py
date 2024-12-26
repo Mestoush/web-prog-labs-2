22edbb61adb7feea35b0903f3a4c9b5b50b767fe
Денис Червоняк <mestoush@mail.ru>
2024-12-26 22:45:46 +0700

    Дополнительное задание

--- lab9.py
+++ lab9.py
@@ -8,61 +8,68 @@ from flask_login import login_user, login_required, current_user, logout_user
 
 lab9 = Blueprint('lab9', __name__)
 
-user_data = {}
-
 @lab9.route('/lab9', methods=['GET', 'POST'])
 def lab92():
+    if 'congratulations' in session:
+        return redirect('/lab9/happycard')
+
     if request.method == 'POST':
-        name = request.form.get('name')  
+        name = request.form.get('name')
         if name:
-            user_data['name'] = name
-            return redirect(url_for('lab9.lab9_age'))  
-        return render_template('lab9/name.html', error="Имя не указано")
-    return render_template('lab9/name.html')
+            session['name'] = name
+            return redirect('/lab9/age')
+    return render_template('lab9/name.html', name=session.get('name', ''))
 
 @lab9.route('/lab9/age', methods=['GET', 'POST'])
 def lab9_age():
     if request.method == 'POST':
-        age = request.form.get('age')  
+        age = request.form.get('age')
         if age:
-            try:
-                user_data['age'] = int(age)
-                return redirect(url_for('lab9.lab9_gender'))  
-            except ValueError:
-                return render_template('lab9/age.html', error="Возраст должен быть числом")
-        return render_template('lab9/age.html', error="Возраст не указан")
-    return render_template('lab9/age.html')
+            session['age'] = int(age)
+            return redirect('/lab9/gender')
+    return render_template('lab9/age.html', age=session.get('age', ''))
 
 @lab9.route('/lab9/gender', methods=['GET', 'POST'])
 def lab9_gender():
     if request.method == 'POST':
-        gender = request.form.get('gender')  
+        gender = request.form.get('gender')
         if gender:
-            user_data['gender'] = gender
-            return redirect(url_for('lab9.lab9_question1'))  
-        return render_template('lab9/gender.html', error="Пол не выбран")
-    return render_template('lab9/gender.html')
+            session['gender'] = gender
+            return redirect('/lab9/question1')
+    return render_template('lab9/gender.html', gender=session.get('gender', ''))
 
 @lab9.route('/lab9/question1', methods=['GET', 'POST'])
 def lab9_question1():
     if request.method == 'POST':
-        choice1 = request.form.get('choice1')  
+        choice1 = request.form.get('choice1')
         if choice1:
-            user_data['choice1'] = choice1
-            return redirect(url_for('lab9.lab9_question2'))  
-        return render_template('lab9/question1.html', error="Выберите вариант")
-    return render_template('lab9/question1.html')
+            session['choice1'] = choice1
+            return redirect('/lab9/question2')
+    return render_template('lab9/question1.html', choice1=session.get('choice1', ''))
 
 @lab9.route('/lab9/question2', methods=['GET', 'POST'])
 def lab9_question2():
     if request.method == 'POST':
-        choice2 = request.form.get('choice2')  
+        choice2 = request.form.get('choice2')
         if choice2:
-            user_data['choice2'] = choice2
-            return redirect(url_for('lab9.lab9_congratulations'))  
-        return render_template('lab9/question2.html', choice1=user_data['choice1'], error="Выберите вариант")
-    return render_template('lab9/question2.html', choice1=user_data['choice1'])
+            session['choice2'] = choice2
+            session['congratulations'] = {
+                'name': session.get('name'),
+                'age': session.get('age'),
+                'gender': session.get('gender'),
+                'choice1': session.get('choice1'),
+                'choice2': session.get('choice2')
+            }
+            return redirect('/lab9/happycard')
+    return render_template('lab9/question2.html', choice1=session.get('choice1', ''))
 
-@lab9.route('/lab9/congratulations', methods=['GET'])
+@lab9.route('/lab9/happycard', methods=['GET'])
 def lab9_congratulations():
-    return render_template('lab9/happycard.html', user_data=user_data)
+    if 'congratulations' not in session:
+        return redirect('/lab9')
+    return render_template('lab9/happycard.html', user_data=session['congratulations'])
+
+@lab9.route('/lab9/reset', methods=['GET'])
+def lab9_reset():
+    session.clear()
+    return redirect('/lab9')
