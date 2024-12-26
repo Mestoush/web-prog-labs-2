579a207df7b3a5d8937a4ac35b434258084ffc04
Денис Червоняк <mestoush@mail.ru>
2024-12-26 17:15:41 +0700

    Логаут

--- lab8.py
+++ lab8.py
@@ -4,7 +4,7 @@ from db.models import users, Articles
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from werkzeug.security import check_password_hash, generate_password_hash
-from flask_login import login_user, login_required, current_user
+from flask_login import login_user, login_required, current_user, logout_user
 
 lab8 = Blueprint('lab8', __name__)
 
@@ -75,3 +75,9 @@ def login():
 @login_required
 def articles_list():
     return 'Список статей'
+
+@lab8.route('/lab8/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/lab8/')
