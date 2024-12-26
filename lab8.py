08e6af49789e8e60e90605b25c9b344c044ba3d8
Денис Червоняк <mestoush@mail.ru>
2024-12-26 17:02:51 +0700

    Авторизация

--- lab8.py
+++ lab8.py
@@ -4,6 +4,7 @@ from db.models import users, Articles
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import login_user, login_required, current_user
 
 lab8 = Blueprint('lab8', __name__)
 
@@ -46,3 +47,31 @@ def register():
     db.session.add(new_user)
     db.session.commit()
     return redirect('/lab8/')
+
+@lab8.route('/lab8/login', methods = ["GET", 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('/lab8/login.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    if not login_form:
+        return render_template('lab8/register.html', error='Имя пользователя не может быть пустым')
+
+    if not password_form:
+        return render_template('lab8/register.html', error='Пароль не может быть пустым')
+
+    user = users.query.filter_by(login = login_form).first()
+
+    if user:
+        if check_password_hash(user.password, password_form):
+            login_user(user, remember = False)
+            return redirect('/lab8/articles')
+        
+    return render_template('/lab8/login.html', error = 'Ошибка входа: логин и/или пароль неверны')
+
+@lab8.route('/lab8/articles/')
+@login_required
+def articles_list():
+    return 'Список статей'
